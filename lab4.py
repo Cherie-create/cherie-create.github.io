7c3850a0e0fe49425addff4fc63ed487f89f3562
Cherie-create <pulcheriembaye11@gmail.com>
2024-08-03 22:42:50 -0400

    Add files via upload

---
+++ lab4.py
@@ -0,0 +1,50 @@
+# ECOR 1041 Lab 4
+
+__author__ = "Pulcherie Mbaye"
+__student_number__ = "101"
+
+import math
+
+# =====================================================
+# Exercise 1
+def area_of_disk(radius:float) -> float:
+    """ Return the area of a disk with the specified radius.
+    precondition: radius >= 0.
+    >>> area_of_disk(5)
+    78.54
+    """
+   
+    return math.pi * radius ** 2
+
+# Type your function definition for Exercise 1 here.
+
+# =======================================================
+# Exercise 2
+LITRES_PER_GALLON = 4.54609 #UPPER CASE FOR CONSTANT VARIABLES
+KMS_PER_MILE = 1.60934 #CONSTANT VARIABLES ARE PLACED BEFORE THE FUNCTION HEADER
+
+def convert_to_litres_per_100_km(mpg: float) -> float:
+    """ Return the convertion of any fuel efficiency measured in mpg to the equivalent metric fuel consumption.
+    precondition: mpg >=0
+    >>> convert_to_litres_per_100_km(5.0)
+    """
+    return #calculation#
+    
+    
+# Type your function definition for Exercise 2 here.
+
+# =======================================================
+# Exercise 3
+
+# Type your function definition for Exercise 3 here.
+
+# =======================================================
+# Exercise 4
+
+# Type your function definition for Exercise 4 here.
+
+# =======================================================
+# Exercise 5
+
+# Type your function definitions for Exercise 5 here.
+
